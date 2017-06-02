1b48f26ab2e3bd18f52df4c7b920195be9ab9822
k3ldar <s1cart3r@gmail.com>
2017-06-02 13:49:55 +0100

    Initially add to source control

---
+++ Dayview/Appointments.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calendar
+{
+    public class AppointmentList : List<Appointment>
+    {
+        public int AppointmentsClash(Appointment appointment)
+        {
+            return (1);
+        }
+    }
+}
---
+++ Dayview/Properties/AssemblyInfo.cs
@@ -0,0 +1,57 @@
+//DayView2 
+//original code based on https://calendar.codeplex.com/
+//
+//modified by Simon Carter (s1cart3r@gmail.com)
+//
+//Redistribution and use in source and binary forms are permitted
+//provided that the above copyright notice and this paragraph are
+//duplicated in all such forms and that any documentation,
+//advertising materials, and other materials related to such
+//distribution and use acknowledge that the software was developed
+//by techcoil.com and Simon Carter.  The name of the
+//techcoil.com and Simon Carter may not be used to endorse or promote products derived
+//from this software without specific prior written permission.
+//THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+//IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+//WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+//change history
+// 09/06/2013 - initial release 
+//
+
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DayView2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DayView2")]
+[assembly: AssemblyProduct("DayView2")]
+[assembly: AssemblyCopyright("Copyright © Retained by Individual Contributors - Original Code Copyright Moreum (2005).")]
+[assembly: AssemblyTrademark("Based on DayView by Moreum")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("08c6be4c-e6ac-42ff-b05c-3efb47df115e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("2.2.0.229")]
+[assembly: System.CLSCompliant(true)]
