e4f6c4a4e0a64baabcc1371b62f3e657a3c09b06
k3ldar <s1cart3r@gmail.com>
2017-08-21 14:14:46 +0200

    Final integration step for Nuget Release from within Visual Studio

--- Dayview/Properties/AssemblyInfo.cs
+++ Dayview/Properties/AssemblyInfo.cs
@@ -53,5 +53,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.2.4.234")]
+[assembly: AssemblyVersion("2.2.6.236")]
 [assembly: System.CLSCompliant(true)]
