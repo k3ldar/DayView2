5e935d9efb05eccf26e8fba8a0197b765898e86e
k3ldar <s1cart3r@gmail.com>
2019-03-03 16:20:02 +0000

    Target multiple frameworks and prep for net core 3 release

--- Dayview/Properties/AssemblyInfo.cs
+++ Dayview/Properties/AssemblyInfo.cs
@@ -53,5 +53,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.2.9.280")]
+[assembly: AssemblyVersion("2.3.0")]
 [assembly: System.CLSCompliant(true)]
+[assembly: AssemblyFileVersion("2.0.0.0")]
